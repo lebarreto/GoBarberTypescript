a13c5348b5415a9b2cf60e6652c1f91e2ec9600d
leticialmb <leticiabarreto196@gmail.com>
2020-04-13 12:57:22 -0400

    Backend gobarber com typescript

---
+++ backend/src/repositories/AppointmentsRepository.ts
@@ -0,0 +1,28 @@
+import { isEqual } from 'date-fns';
+import Appointments from '../models/Appointments';
+
+class AppointmentsRepository {
+  private appointments: Appointments[];
+
+  constructor() {
+    this.appointments = [];
+  }
+
+  public findByDate(date: Date): Appointments | null {
+    const findAppointment = this.appointments.find(appointment =>
+      isEqual(date, appointment.date),
+    );
+
+    return findAppointment || null;
+  }
+
+  public create(provider: string, date: Date): Appointments {
+    const appointment = new Appointments(provider, date);
+
+    this.appointments.push(appointment);
+
+    return appointment;
+  }
+}
+
+export default AppointmentsRepository;
