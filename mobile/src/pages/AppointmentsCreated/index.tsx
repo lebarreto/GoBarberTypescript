6d7ef72f42552a13c53bf94e4ed62cda39ed042a
leticialmb <leticiabarreto196@gmail.com>
2020-06-18 10:11:40 -0400

    Criação e estilização da dashboard

---
+++ mobile/src/pages/AppointmentsCreated/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const AppointmnetsCreated: React.FC = () => {
+  return <View />;
+};
+
+export default AppointmnetsCreated;
--- mobile/src/pages/Dashboard/index.tsx
+++ mobile/src/pages/Dashboard/index.tsx
@@ -1,15 +1,95 @@
-import React from 'react';
-import { View, Button } from 'react-native';
+import React, { useEffect, useState, useCallback } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import { useAuth } from '../../hooks/auth';
+import api from '../../services/api';
+import {
+  Container,
+  Header,
+  HeaderTitle,
+  Username,
+  ProfileButton,
+  UserAvatar,
+  ProvidersList,
+  ProviderContainer,
+  ProviderAvatar,
+  ProviderInfo,
+  ProviderName,
+  ProviderMeta,
+  ProviderMetaText,
+  ProvidersListTitle,
+} from './styles';
+
+export interface Provider {
+  id: string;
+  name: string;
+  avatar_url: string;
+}
 
 const Dashboard: React.FC = () => {
-  const { signOut } = useAuth();
+  const { signOut, user } = useAuth();
+  const navigation = useNavigation();
+
+  const [providers, setProviders] = useState<Provider[]>([]);
+
+  useEffect(() => {
+    api.get('providers').then((response) => {
+      setProviders(response.data);
+    });
+  }, []);
+
+  const navigateToCreateAppointment = useCallback(
+    (providerId: string) => {
+      navigation.navigate('CreateAppointment', {
+        providerId,
+      });
+    },
+    [navigation],
+  );
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center' }}>
-      <Button title="Sair" onPress={signOut} />
-    </View>
+    <Container>
+      <Header>
+        <HeaderTitle>
+          Bem vindo, {'\n'}
+          <Username>{user.name}</Username>
+        </HeaderTitle>
+
+        <ProfileButton onPress={() => navigation.navigate('Profile')}>
+          <UserAvatar source={{ uri: user.avatar_url }} />
+        </ProfileButton>
+      </Header>
+
+      <ProvidersList
+        data={providers}
+        ListHeaderComponent={
+          <ProvidersListTitle>Cabeleireiros</ProvidersListTitle>
+        }
+        keyExtractor={(provider) => provider.id}
+        renderItem={({ item: provider }) => (
+          <ProviderContainer
+            onPress={() => navigateToCreateAppointment(provider.id)}
+          >
+            <ProviderAvatar source={{ uri: provider.avatar_url }} />
+
+            <ProviderInfo>
+              <ProviderName>{provider.name}</ProviderName>
+
+              <ProviderMeta>
+                <Icon name="calendar" size={14} color="#ff9000" />
+                <ProviderMetaText>Segunda à sexta</ProviderMetaText>
+              </ProviderMeta>
+
+              <ProviderMeta>
+                <Icon name="clock" size={14} color="#ff9000" />
+                <ProviderMetaText>8h às 18h</ProviderMetaText>
+              </ProviderMeta>
+            </ProviderInfo>
+          </ProviderContainer>
+        )}
+      />
+    </Container>
   );
 };
 
